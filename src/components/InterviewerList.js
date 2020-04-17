7f469995b8fd7b5a5a31885cfc37e2cb31e22398
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-04-16 18:12:50 -0700

    completed InterviewList and storybooked it

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,28 @@
 import React from "react";
 import classnames from "classnames";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList({
   interviewers,
   interviewer,
   setInterviewer,
 }) {
-  return <li></li>;
+  const interviewerClass = classnames("interviewers");
+  const persons = interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={() => setInterviewer(interviewer.id)}
+      />
+    );
+  });
+  return (
+    <section className={interviewerClass}>
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{persons}</ul>
+    </section>
+  );
 }
-//
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -15,7 +15,7 @@ export default function InterviewerListItems({
   // li gets interviewersClass as it contains entire image and name
   // image tag only needs to worry about itself, NOT THE NAME AS WELL
   return (
-    <li className={interviewerClass} onClick={() => setInterviewer()}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img className="interviewers__item-image" src={avatar} alt={name}></img>
       {selected && name}
     </li>
