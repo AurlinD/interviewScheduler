7f617043a1aeeaed384f19b990d9d82106b878ff
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-04-22 10:33:38 -0700

    created and tested getAppointmentsForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function getAppointmentsForDay(state, day) {
+  const result = [];
+
+  const filteredDay = state.days.filter((sched) => sched.name === day);
+
+  if (filteredDay[0]) {
+    let apps = filteredDay[0].appointments;
+
+    for (let i = 0; i < apps.length; i++) {
+      for (const appointment in state.appointments) {
+        if (apps[i] == Number(appointment))
+          result.push(state.appointments[appointment]);
+      }
+    }
+
+    return result;
+  } else {
+    return result;
+  }
+}
