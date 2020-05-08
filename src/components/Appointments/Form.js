0ba6f03cf4612600bf77a8ab02d23afe74d38bc2
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-05-08 16:18:23 -0700

    added errors if name and interview unspecified

--- src/components/Appointments/Form.js
+++ src/components/Appointments/Form.js
@@ -1,11 +1,12 @@
-import "./styles.scss";
 import React, { useState } from "react";
-import InterviewerList from "../InterviewerList";
-import Button from "../Button";
+import InterviewerList from "../InterviewerList.js";
+import Button from "../Button.js";
 
 export default function Form(props) {
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [nameError, setNameError] = useState("");
+  const [interviewerError, setInterviewerError] = useState("");
 
   const reset = () => {
     setName("");
@@ -18,6 +19,22 @@ export default function Form(props) {
   };
 
   const onSave = () => {
+    if (!name || !interviewer) {
+      if (name === "") {
+        setNameError("Student name cannot be blank");
+      } else {
+        setNameError("");
+      }
+      if (!interviewer) {
+        setInterviewerError("Please select an interviewer");
+      } else {
+        setInterviewerError("");
+      }
+      return;
+    }
+
+    setNameError("");
+    setInterviewerError("");
     props.onSave(name, interviewer);
   };
 
@@ -28,25 +45,31 @@ export default function Form(props) {
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
+            data-testid="student-name-input"
+            value={name}
             type="text"
             placeholder="Enter Student Name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            data-testid="student-name-input"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           />
+          <section className="appointment__validation">{nameError}</section>
+          <InterviewerList
+            interviewers={props.interviewers}
+            interviewer={interviewer}
+            setInterviewer={setInterviewer}
+          />
+          <section className="appointment__validation">
+            {interviewerError}
+          </section>
         </form>
-        <InterviewerList
-          interviewers={props.interviewers}
-          interviewer={interviewer}
-          setInterviewer={setInterviewer}
-        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={cancel} danger>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button onClick={onSave} confirm>
+          <Button confirm onClick={onSave}>
             Save
           </Button>
         </section>
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,11 +1,26 @@
-import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
 import Appointment from "components/Appointments/index";
 
-afterEach(cleanup);
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
+import React from "react";
 
-it("renders without crashing", () => {
-  render(<Appointment />);
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
+import { render } from "@testing-library/react";
+
+/*
+  We import the component that we are testing
+*/
+import Application from "components/Application";
+
+/*
+  A test that renders a React Component
+*/
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
